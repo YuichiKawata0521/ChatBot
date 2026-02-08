b5fa6935423d5c43cdce08b83c4405e078527171
YuichiKawata0521 <hetianyouyi235@gmail.com>
2026-02-08 14:58:48 +0900

    Add: .js extension in import

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,4 +1,4 @@
-import { connectDB } from "./config/db";
+import { connectDB } from "./config/db.js";
 
 const PORT = process.env.BACKEND_PORT || 3000;
 
--- backend/src/utils/password.js
+++ backend/src/utils/password.js
@@ -1,5 +1,5 @@
 import argon2 from 'argon2';
-import { PASSWORD_PEPPER, ARGON2_OPTIONS } from '../config/security';
+import { PASSWORD_PEPPER, ARGON2_OPTIONS } from '../config/security.js';
 
 export const hashPassword = async (plainPassword) => {
     const pepperedPassword = plainPassword + PASSWORD_PEPPER;
