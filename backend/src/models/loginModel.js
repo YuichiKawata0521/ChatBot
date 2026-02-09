37a5f95fb278b2f4c7de5afae1f7d3435884f922
YuichiKawata0521 <hetianyouyi235@gmail.com>
2026-02-09 23:08:06 +0900

    Add: register

--- backend/src/models/loginModel.js
+++ backend/src/models/loginModel.js
@@ -1,12 +1,24 @@
-export const getUserData = async (pool, employee_no, email) => {
+ export const getUserData = async (pool, employee_no, email) => {
     const sql = `
         SELECT *
         FROM users
         WHERE employee_no = $1 AND email = $2;
     `;
     const result = await pool.query(sql, [employee_no, email]);
-    return result.rows[0];
+    return result.rowCount;
 }
+
+export const register = async (pool, data) => {
+    const { employee_no, user_name, email, password, department_id, role } = data;
+    
+    const sql = `
+        INSERT INTO users (employee_no, user_name, email, password, department_id, role)
+        VALUES ($1, $2, $3, $4, $5, $6);
+    `;
+
+    await pool.query(sql, [employee_no, user_name, email, password, department_id, role]);
+    return {success: true};
+} 
 
 export const saveResetToken = async (pool, id, token, expires) => {
     const sql = `
