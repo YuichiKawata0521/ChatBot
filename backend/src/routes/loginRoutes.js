ffc67fcd3c6dd277a307908617da5bca445d2b92
YuichiKawata0521 <hetianyouyi235@gmail.com>
2026-02-09 23:35:14 +0900

    Add: logout, me, register routes

--- backend/src/routes/loginRoutes.js
+++ backend/src/routes/loginRoutes.js
@@ -6,3 +6,9 @@ import { protect } from '../middlewares/authMiddleware.js';
 const router = express.Router();
 
 router.post("/login", loginLimiter, loginController.login);
+
+router.post('/logout', protect, loginController.logout);
+
+router.get('/me', protect, loginController.authMe);
+
+router.post('/register', protect, loginController.register);
