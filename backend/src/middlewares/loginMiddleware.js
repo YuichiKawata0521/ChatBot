46afb1e9919fd28c52d717ac7a3f8818293d69e7
YuichiKawata0521 <hetianyouyi235@gmail.com>
2026-02-12 23:17:18 +0900

    Update: switch loginLimit max

--- backend/src/middlewares/loginMiddleware.js
+++ backend/src/middlewares/loginMiddleware.js
@@ -1,8 +1,15 @@
 import rateLimit from 'express-rate-limit';
 
+let maxTry = 0;
+if (process.env.NODE_ENV === 'production') {
+    maxTry = 5;
+} else {
+    maxTry = 500;
+}
+
 const loginLimiter = rateLimit({
     windowMs: 15 * 60 * 1000,
-    max: 5,
+    max: maxTry,
     message: {
         status: 429,
         message: 'ログインの失敗が続きました。15分後に再度お試しください。',
