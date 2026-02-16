55e1cca90ef7cc5f1961287458286dbb912da964
YuichiKawata0521 <hetianyouyi235@gmail.com>
2026-02-16 15:05:48 +0900

    Add: req.user in protect

--- backend/src/middlewares/authMiddleware.js
+++ backend/src/middlewares/authMiddleware.js
@@ -3,6 +3,7 @@ import AppError from '../utils/appError.js';
 // ログイン状態の確認
 export const protect = (req, res, next) => {
     if (req.session.user) {
+        req.user = req.session.user;
         return next();
     }
     return next(new AppError('ログイン認証が必要です。', 401));
