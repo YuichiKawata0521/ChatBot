a14662c8f766a2f00197035b65fded40b341b369
YuichiKawata0521 <hetianyouyi235@gmail.com>
2026-02-17 22:48:27 +0900

    Add: endpoint of /embed/query

--- llm/src/routers/embed.py
+++ llm/src/routers/embed.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
-from src.models.embed import EmbedRequest, EmbedResponse
-from src.services.embed_service import process_embedding
+from src.models.embed import EmbedRequest, EmbedResponse, QueryEmbedRequest, QueryEmbedResponse
+from src.services.embed_service import process_embedding, get_query_embedding
 
 router = APIRouter()
 
@@ -8,3 +8,8 @@ router = APIRouter()
 async def embed_ednpoint(req: EmbedRequest):
     chunks = await process_embedding(req.text, req.chunk_size, req.chunk_over_lap)
     return EmbedResponse(chunks=chunks)
+
+@router.post("/embed/query", response_model=QueryEmbedResponse)
+async def embed_query_endpoint(req: QueryEmbedRequest):
+    embedding = await get_query_embedding(req.text)
+    return QueryEmbedResponse(embedding=embedding)
