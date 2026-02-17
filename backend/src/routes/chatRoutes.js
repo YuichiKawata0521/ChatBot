3d516b28929461afdb720e785ea319d6a5c92b10
YuichiKawata0521 <hetianyouyi235@gmail.com>
2026-02-17 22:44:40 +0900

    Add: documents routes

--- backend/src/routes/chatRoutes.js
+++ backend/src/routes/chatRoutes.js
@@ -1,12 +1,16 @@
 import { chatController } from "../controllers/chatController.js";
+import * as documentController from '../controllers/documentController.js';
 import express from 'express';
 import { protect } from '../middlewares/authMiddleware.js';
 
 const router = express.Router();
 
-router.post('/', protect, chatController.sendMessage);
-router.get('/threads', protect, chatController.getThreads);
-router.post('/delete-history', protect, chatController.deleteAllThreads);
-router.get('/:threadId', protect, chatController.getHistory);
+router.use(protect);
+
+router.post('/', chatController.sendMessage);
+router.get('/threads', chatController.getThreads);
+router.post('/delete-history', chatController.deleteAllThreads);
+router.get('/documents', documentController.getDocuments);
+router.get('/:threadId', chatController.getHistory);
 
 export default router;
