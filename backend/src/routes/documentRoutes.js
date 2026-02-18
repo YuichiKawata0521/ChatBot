63aca9879a32d351543eb6de700be4cdf9409d97
YuichiKawata0521 <hetianyouyi235@gmail.com>
2026-02-18 22:35:03 +0900

    Add: upload route

--- backend/src/routes/documentRoutes.js
+++ backend/src/routes/documentRoutes.js
@@ -1,13 +1,18 @@
 import express from 'express';
+import multer from 'multer';
 import * as documentController from '../controllers/documentController.js';
 import { authorizeRoles, protect } from '../middlewares/authMiddleware.js';
 
 const router = express.Router();
+const upload = multer({ dest: 'uploads/temp/' });
 
 router.use(protect); // 認証必須
+router.use(authorizeRoles('admin'));
 
 router.route('/')
     .get(documentController.getDocuments)
-    .post(authorizeRoles('admin'), documentController.createDocument); // 登録は管理者のみ
+    .post(documentController.createDocument); // 登録は管理者のみ
+
+router.post('/upload', upload.single('file'), documentController.uploadDocument);
 
 export default router;
