0109170f3e5a4b0ada45cedb86cee7a816066b35
YuichiKawata0521 <hetianyouyi235@gmail.com>
2026-02-27 17:07:11 +0900

    Add: document manage routes

--- backend/src/routes/documentRoutes.js
+++ backend/src/routes/documentRoutes.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import multer from 'multer';
 import * as documentController from '../controllers/documentController.js';
+import * as docManageController from '../controllers/docManageController.js';
 import { authorizeRoles, protect } from '../middlewares/authMiddleware.js';
 
 const router = express.Router();
@@ -13,6 +14,11 @@ router.route('/')
     .get(documentController.getDocuments)
     .post(documentController.createDocument); // 登録は管理者のみ
 
+router.get('/:id', docManageController.getDocContent);
+
 router.post('/upload', upload.single('file'), documentController.uploadDocument);
 
+router.post('/delete/:id', docManageController.deleteDocument)
+router.post('/file/:id', upload.single('file'), docManageController.reUploadDocument);
+router.post('/rename/:id', docManageController.updateTitle);
 export default router;
